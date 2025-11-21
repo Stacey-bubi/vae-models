91a586a8a725c4718d78ff093bf4331be62ab275
Slava Chaunin <67190162+Ssslakter@users.noreply.github.com>
2025-11-21 14:35:08 +0300

    add colored dataset

--- vae_project/dataset/mnist.py
+++ vae_project/dataset/mnist.py
@@ -1,4 +1,3 @@
-# %%
 from typing import Callable
 from functools import partial
 import torch as t
@@ -6,26 +5,16 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms as tfm
 
 
-def colorize(img: t.Tensor, label: t.Tensor):
-    "Colorizes a single-channel image with a random color."
-    c = t.rand(3, 1, 1)  # random RGB color
-    return img * c
-
-
 def get_dataset(
     root: str = "./data",
     train: bool = True,
     transforms: list = None,
-    colorize_fn: Callable = None,
+    colored: bool = True,
 ):
     "Get a transformed MNIST dataset, optionally a colored one."
     transforms = transforms or []
     transforms.append(tfm.ToTensor())
-    cls = (
-        datasets.MNIST
-        if colorize_fn is None
-        else partial(ColoredMNIST, colorize_fn=colorize_fn)
-    )
+    cls = datasets.MNIST if not colored else ColoredMNIST
     return cls(root=root, train=train, download=True, transform=tfm.Compose(transforms))
 
 
@@ -42,18 +31,25 @@ def to_dataloaders(train_dataset, val_dataset=None, batch_size=32, val_batch_siz
 class ColoredMNIST(datasets.MNIST):
     "An MNIST dataset that applies a `colorize_fn` to each image."
 
-    def __init__(self, root, colorize_fn, train=True, transform=None, download=False):
+    def __init__(self, root, train=True, transform=None, download=False, random_colors=False):
         super().__init__(root, train, transform, download=download)
-        self.colorize_fn = colorize_fn
+        self.colorize_fn = self.colorize_rand if random_colors else self.colorize_cls
+        self.n_classes = len(self.classes)
 
     def __getitem__(self, idx):
         img, label = super().__getitem__(idx)
         img = self.colorize_fn(img, label)
         return img, label
-# %%
 
-ds = get_dataset("../data", colorize_fn=colorize)
-# %%
 
-dl = DataLoader(ds, 4)
-xb, yb = next(iter(dl))
+    def colorize_rand(self, img: t.Tensor, label: t.Tensor):
+        "Colorizes a single-channel image with a random color."
+        c = t.rand(img.shape[0], 3, 1, 1)  # random RGB color
+        return img * c
+
+
+    def colorize_cls(self, img: t.Tensor, label: t.Tensor):
+        "Colorizes a single-channel image based on class label."
+        h = t.as_tensor(label, device=img.device).float()/self.n_classes*6
+        c = t.stack([(h-3).abs()-1, 2-(h-2).abs(), 2-(h-4).abs()], -1).clamp(0,1)
+        return img * c[...,None,None]
---
+++ vae_project/utils.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
+import torch as t
+
+
+def show_img(x: t.Tensor, ax=None):
+    if ax is None: ax = plt.subplot()
+    ax.imshow(x.cpu().detach().permute(1,2,0))
+    return ax
+
+
+def show_imgs(xb: t.Tensor, ax=None):
+    nrow = int(xb.shape[0]**0.5)
+    xb = make_grid(xb, nrow=nrow, pad_value=1)
+    return show_img(xb, ax)
